930b0fa6be0962b41147a6a4bb60caef307d75d1
M. Manousos <mmanousos@gmail.com>
2018-04-09 14:32:05 -0700

    Adds ResetGame functionality

--- script.js
+++ script.js
@@ -1,29 +1,51 @@
 
-const compPlays = [];
-const userPlays = [];
-const compCurrrent = '';
-const userCurrent = '';
-const count = 0;
+$(document).ready(function(){
 
+	// global variables
 
+	const compPlays = [];
+	const userPlays = [];
+	const compCurrrent = '';
+	const userCurrent = '';
+	const count = 0;
 
-$('#start').on('click', function() {
-	if ($('#start-text').hasClass('reset')) {
-		$('.start-text').removeClass('reset');
-		$('.start-text').text('start');
-	} else {
-		$('#start-text').addClass('reset');
-		$('.start-text').text('reset');
+	// reset game function
+	const ResetGame = function() {
+		// reset display-text to 00
+		$('#display-text').text('00');
+		// reset placeholders
+		compPlays = [];
+		userPlays = [];
+		compCurrrent = '';
+		userCurrent = '';
+		count = 0;
 	}
-})
 
-$('#strict').on('click', function() {
-	if ($('#strict-indicate').hasClass('on')) {
-		$('#strict').attr('aria-pressed', 'false');
-		$('#strict-indicate').removeClass('on');
-	} else {
-		$('#strict').attr('aria-pressed', 'true');
-		$('#strict-indicate').addClass('on');
+	// start / reset button
+	$('#start').on('click', function() {
+		if ($('#start-text').hasClass('reset')) {
+			$('.start-text').removeClass('reset');
+			$('.start-text').text('start');
+			ResetGame();
+		} else {
+			$('#start-text').addClass('reset');
+			$('.start-text').text('reset');
+			$('#display-text').text('01');
+		}
+	})
 
-	}
-})
+
+	// strict button
+	$('#strict').on('click', function() {
+		if ($('#strict-indicate').hasClass('on')) {
+			$('#strict').attr('aria-pressed', 'false');
+			$('#strict-indicate').removeClass('on');
+		} else {
+			$('#strict').attr('aria-pressed', 'true');
+			$('#strict-indicate').addClass('on');
+		}
+	})
+
+
+
+});
